da7c9894119b645dc9b1e312f7f6c7ca505ea5fa
dariovazano <dario.vazano@hotmail.com>
2023-04-15 17:24:01 +0200

    creando el repositorio

---
+++ app.js
@@ -0,0 +1,21 @@
+// let salario = prompt("cuanto cobras?")
+
+
+// if (salario>500000){
+//     alert("sos millo")
+// }else if ((salario<500000) && (salario>150000)){
+//     alert("capaz llegas a fin de mes")
+// }
+// else{
+//     alert("buscate un segundo laburo hermano")
+// }
+
+function gastos(alquiler, comida, nafta, joda){
+this.alquiler=alquiler=prompt("cuanto pagas de alquiler")
+this.comida=comida=prompt("cuanto gastas en comida")
+this.nafta=nafta=prompt("cuanto gastas en nafta")
+this.joda=joda=prompt("cuanto gastas en joda")
+}
+
+const gastos1=new gastos()
+console.log(gastos1)
